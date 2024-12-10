547999243397cd8c90ca92738785c2aa50bf1a08
DoomZyx <axelcella.ac@gmail.com>
2024-12-10 08:18:35 +0100

    Scrollbar hidden on homepage

--- react/FrontEnd/src/pages/Home/Homepage.jsx
+++ react/FrontEnd/src/pages/Home/Homepage.jsx
@@ -2,8 +2,18 @@ import Logo from "../../components/Homepage-components/Logo/Logo.jsx";
 import Header from "../../components/Homepage-components/Header-fix/Header.jsx";
 import Nav from "../../components/Homepage-components/Nav/Nav.jsx";
 import Footer from "../../components/Homepage-components/Footer/Footer.jsx";
+import { useEffect } from "react";
 
 function HomePage() {
+  useEffect(() => {
+
+    document.body.style.overflow = "hidden";
+
+    return () => {
+
+      document.body.style.overflow = "auto";
+    };
+  }, []);
   return (
     <div className="homepage-body">
       <Header />
